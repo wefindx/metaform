e93edfacd66bf1f77e8cc12c2e10cb5a8650e1cd
Mindey <mindey@qq.com>
2018-08-18 22:01:58 +0000

    more converters

--- metaform/converters.py
+++ metaform/converters.py
@@ -1,8 +1,18 @@
 from boltons.timeutils import isoparse
 from urllib.parse import urlparse
+from decimal import Decimal
 
 def object(x):
     return dict(x)
+
+def integer(x):
+    return int(x)
+
+def rational(x):
+    return float(x)
+
+def decimal(x):
+    return decimal(x)
 
 def string(x):
     return str(x)
